d335153a4124b22476409ebc5169255c2ad0295d
Eric Martinez <eric@ericmartinez.dev>
2023-07-01 01:18:20 -0400

    example of how getAttribute works

--- component.js
+++ component.js
@@ -2,24 +2,7 @@ class WebComponent extends HTMLElement {
   constructor() {
     super();
     
-    // 1. By simply setting the innerHTML
-    // this.innerHTML = `
-    //   <div>
-    //     <span>This is a web component</span>
-    //   </div>
-    // `;
-
-    // 2. By creating elements and adding them to the DOM
-    // const div = document.createElement('div');
-    // const span = document.createElement('span');
-    // span.innerHTML = "This is a web component";
-    // div.appendChild(span);
-    // this.appendChild(div);
-
-    // 3. Parse a HTML string
-    const html = '<div><span>This is a web component</span></div>';
-    const innerHTML = new DOMParser().parseFromString(html, 'text/html').body.innerHTML;
-    this.innerHTML = innerHTML;
+    console.log(this.getAttribute('text'));
   }
 }
 
