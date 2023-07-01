40c848763745edccf770b39e687db4b18a292049
Eric Martinez <eric@ericmartinez.dev>
2023-07-01 02:09:38 -0400

    Custom Elements p2 - Main lifecycle methods

--- component.js
+++ component.js
@@ -1,6 +1,11 @@
 class WebComponent extends HTMLElement {
+  
+  // 1. Constructor - created in memory but not necessarily attached to the DOM yet
+  // This is best place for initilisation but not the best place for interacting with the DOM
   constructor() {
     super();
+
+    console.log('constructor')
     
     if(this.hasAttribute('text')) {
       this.innerTextContent = this.getAttribute('text');
@@ -12,6 +17,13 @@ class WebComponent extends HTMLElement {
       </div>
     `;
   }
+
+  // 2. Connected Callback - when the element is attached to the DOM
+  connectedCallback() {
+    console.log('Connected Callback')
+  }
+
+  // 3. Attribute Changed Callback - checking if an attribute has changed
   attributeChangedCallback(attrName, oldValue, newValue) {
     if(attrName == 'text') {
       this.innerHTML = `
@@ -20,6 +32,12 @@ class WebComponent extends HTMLElement {
         </div>
       `;
     }
+  }
+
+  // 4. Disconnected Callback - When the element is detached from the DOM
+  // Useful for performing cleanup
+  disconnectedCallback() {
+    console.log('Disconnected Callback')
   }
 
   static get observedAttributes() {
