6268a98d986a333b9f0a9031e64654c4287d9b93
Eric Martinez <eric@ericmartinez.dev>
2023-07-01 00:55:32 -0400

    initial commit

---
+++ component.js
@@ -0,0 +1,14 @@
+class WebComponent extends HTMLElement {
+  constructor() {
+    super();
+    
+    // 1. By simply setting the innerHTML
+    this.innerHTML = `
+      <div>
+        <span>This is a web component</span>
+      </div>
+    `;
+  }
+}
+
+customElements.define('web-component', WebComponent);
