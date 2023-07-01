7e85aba155bd0ca6a3d7b3f9ae82715128356d7e
Eric Martinez <eric@ericmartinez.dev>
2023-07-01 01:09:52 -0400

    creating elements with and adding them to the DOM

--- component.js
+++ component.js
@@ -3,11 +3,18 @@ class WebComponent extends HTMLElement {
     super();
     
     // 1. By simply setting the innerHTML
-    this.innerHTML = `
-      <div>
-        <span>This is a web component</span>
-      </div>
-    `;
+    // this.innerHTML = `
+    //   <div>
+    //     <span>This is a web component</span>
+    //   </div>
+    // `;
+
+    // 2. By creating elements and adding them to the DOM
+    const div = document.createElement('div');
+    const span = document.createElement('span');
+    span.innerHTML = "This is a web component";
+    div.appendChild(span);
+    this.appendChild(div);
   }
 }
 
